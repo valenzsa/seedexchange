e83cf001cea8edb305c0ea67f85e2f6551262ca0
valenzsa <valenzsa418@gmail.com>
2024-05-18 23:20:33 -0400

    Update products mock data to include more sample products

--- client/src/products.js
+++ client/src/products.js
@@ -1,43 +1,106 @@
 const products = [
   {
     id: "1",
-    name: "Tomato",
-    description: "This is a tomato",
-    image: "https://placehold.co/300x300/black/white",
+    name: "Sheboygan Tomato",
+    description:
+      "Introduced into the 1998 Yearbook by Seed Savers Exchange member Jeff Nekola. Grown since the early 1900s by Lithuanian immigrants in Sheboygan, Wisconsin. Pink paste-type 4-6 ounce fruits. Excellent flavor. Heavy yields, great for canning.",
+    image:
+      "https://shop.seedsavers.org/site/img/seo-images/1484-sheboygan-tomato-organic.jpg",
     oldPrice: 2.99,
     price: 1.99,
     rating: 4.5,
     numberOfReviews: 21,
+    category: "tomato",
   },
   {
     id: "2",
     name: "Okra",
-    description: "This is an okra",
-    image: "https://placehold.co/300x300/magenta/white",
+    description:
+      'The straight, green, downy pods of this variety are intermediate in length and width, and are best eaten when under 5" long. Seed Savers Exchange member Dr. James Wolfe received the variety from the Simpson family of Rogersville, Tennessee. D.V. (Simpson) Thomas remembers her mother, Nora, growing this okra as far back as the early 1940s.',
+    image:
+      "https://shop.seedsavers.org/site/img/seo-images/0161-simpson-okra-organic.jpg",
     oldPrice: 3.99,
     price: 2.99,
     rating: 4.5,
     numberOfReviews: 11,
+    category: "okra",
   },
   {
     id: "3",
-    name: "Cucumber",
-    description: "This is an cucumber",
-    image: "https://placehold.co/300x300/green/white",
+    name: "Japanese Climbing Cucumber",
+    description:
+      'This Japanese climbing variety bears tender, crisp, and slightly tart 9"-long cucumbers that are excellent for slicing or pickling. Listed by J.M. Thornton & Company in 1894, this variety’s strong grasping tendrils make it ideal for growing on a trellis or fence, but it can also be grown on the ground.',
+    image:
+      "https://shop.seedsavers.org/site/img/seo-images/1191-japanese-climbing-cucumber-organic.jpg",
     oldPrice: 1.99,
     price: 0.99,
     rating: 3.5,
     numberOfReviews: 4,
+    category: "cucumber",
   },
   {
     id: "4",
-    name: "Eggplant",
-    description: "This is an eggplant",
-    image: "https://placehold.co/300x300/pink/white",
+    name: "Pingtung Long Eggplant",
+    description: "",
+    image:
+      "https://shop.seedsavers.org/site/img/seo-images/0620-pingtung-long-eggplant-organic.jpg",
     oldPrice: 5.99,
     price: 1.99,
     rating: 2.5,
     numberOfReviews: 2,
+    category: "eggplant",
+  },
+  {
+    id: "5",
+    name: "Black Cherry Tomato",
+    description:
+      'Vigorous regular leaf plants covered in clusters of 1" round cherry tomatoes. Dusky purple-black fruits bursting with rich flavor—sweet and complex.',
+    image:
+      "https://shop.seedsavers.org/site/img/seo-images/1609-black-cherry-tomato-organic.jpg",
+    oldPrice: 2.99,
+    price: 1.99,
+    rating: 3.5,
+    numberOfReviews: 2,
+    category: "tomato",
+  },
+  {
+    id: "6",
+    name: "Shishito Pepper",
+    description:
+      "Compact, bushy plants produce an abundance of 3.5-4\"\" wrinkled fruits over an extended period. When eaten in the green stage, as is traditional in Japan, peppers are incredibly crunchy with just a hint of heat. When red, the fruits are sweet and slightly hotter but not as crunchy. Perfect for frying, this Japanese heirloom's fruits are usually mild, but an occasional pepper (about one in 10) is hot. Kitazawa Seed Company played an instrumental role in introducing 'Shishito' to the United States seed trade; Seed Savers Exchange acquired the variety in the 1980s from the Urban Farmerseed company of Convent Station, N.J.",
+    image:
+      "https://shop.seedsavers.org/site/img/seo-images/0286-shishito-pepper.jpg",
+    oldPrice: 2.99,
+    price: 1.99,
+    rating: 3.5,
+    numberOfReviews: 5,
+    category: "pepper",
+  },
+  {
+    id: "7",
+    name: "Burgess Buttercup Squash",
+    description:
+      "Introduced in 1952 by Burgess Seed & Plant Co. This is a bush version of the original Buttercup variety introduced in 1931. Flattened dark green turbans with a distinctive button on the blossom end. Typical fruits weigh 3-5 pounds. Super sweet brilliant orange flesh with very fine eating qualities. Rind is thin but very hard, medium length keeper.",
+    image:
+      "https://shop.seedsavers.org/site/img/seo-images/1344-burgess-buttercup-squash-organic.jpg",
+    oldPrice: 2.99,
+    price: 1.99,
+    rating: 3.5,
+    numberOfReviews: 1,
+    category: "squash",
+  },
+  {
+    id: "8",
+    name: "Bloomsdale Spinach",
+    description:
+      "Developed in 1874 by D. Landreth & Sons seed company of Philadelphia. Vigorous upright plants with dark green, glossy, savoyed leaves. Fine quality, very tender, excellent flavor. Quick growing variety with heavy yields. Well adapted for late spring or summer plantings, slow to bolt.",
+    image:
+      "https://shop.seedsavers.org/site/img/seo-images/0656-Bloomsdale-spinach-Organic.jpg",
+    oldPrice: 2.99,
+    price: 1.99,
+    rating: 4.5,
+    numberOfReviews: 4,
+    category: "spinach",
   },
 ];
 
