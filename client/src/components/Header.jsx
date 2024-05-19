091b29bf554cd3cb5a2ccfb49861be9977b31012
valenzsa <valenzsa418@gmail.com>
2024-05-19 00:16:32 -0400

    Add flower seeds menu item in the header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -19,7 +19,18 @@ const Header = () => {
             style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <NavDropdown title="Seeds" id="navbarScrollingDropdown">
+            <NavDropdown title="Vegetable Seeds" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown title="Flower Seeds" id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action4">
                 Another action
