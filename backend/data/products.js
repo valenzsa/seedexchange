30548b83b906de7f0b4cdd64d4abf09216b067d3
valenzsa <valenzsa418@gmail.com>
2024-05-22 15:28:48 -0400

    Duplicated products mock data into backend data directory

---
+++ backend/data/products.js
@@ -0,0 +1,146 @@
+const products = [
+  {
+    id: "1",
+    name: "Sheboygan Tomato",
+    description:
+      "Introduced into the 1998 Yearbook by Seed Savers Exchange member Jeff Nekola. Grown since the early 1900s by Lithuanian immigrants in Sheboygan, Wisconsin. Pink paste-type 4-6 ounce fruits. Excellent flavor. Heavy yields, great for canning.",
+    images: [
+      "https://i.etsystatic.com/28841995/r/il/ba9eeb/3475483670/il_794xN.3475483670_jyqa.jpg",
+    ],
+    oldPrice: 2.99,
+    price: 1.99,
+    rating: 4.5,
+    numberOfReviews: 21,
+    numberOfSeedsPerPack: 25,
+    seedCount: 75,
+    typeOfPlant: "Vegetable",
+    category: "Tomato",
+    inStock: true,
+  },
+  {
+    id: "2",
+    name: "Clemson Spineless Okra Seeds",
+    description:
+      'The straight, green, downy pods of this variety are intermediate in length and width, and are best eaten when under 5" long. Seed Savers Exchange member Dr. James Wolfe received the variety from the Simpson family of Rogersville, Tennessee. D.V. (Simpson) Thomas remembers her mother, Nora, growing this okra as far back as the early 1940s.',
+    images: [
+      "https://i.etsystatic.com/26542041/r/il/c8fb48/2963819818/il_794xN.2963819818_t79k.jpg",
+      "https://i.etsystatic.com/26542041/r/il/9e8a95/3008917485/il_794xN.3008917485_qfg7.jpg",
+      "https://i.etsystatic.com/26542041/r/il/b27a57/2961212226/il_794xN.2961212226_6wme.jpg",
+      "https://i.etsystatic.com/26542041/r/il/6c0b03/3008917349/il_794xN.3008917349_6fy7.jpg",
+    ],
+    oldPrice: 3.99,
+    price: 2.99,
+    rating: 4.5,
+    numberOfReviews: 11,
+    numberOfSeedsPerPack: 10,
+    seedCount: 35,
+    typeOfPlant: "Vegetable",
+    category: "Okra",
+  },
+  {
+    id: "3",
+    name: "Japanese Climbing Cucumber",
+    description:
+      'This Japanese climbing variety bears tender, crisp, and slightly tart 9"-long cucumbers that are excellent for slicing or pickling. Listed by J.M. Thornton & Company in 1894, this variety’s strong grasping tendrils make it ideal for growing on a trellis or fence, but it can also be grown on the ground.',
+    images: [
+      "https://i.etsystatic.com/19374939/r/il/c5bf86/2368194912/il_794xN.2368194912_qnkx.jpg",
+    ],
+    oldPrice: 1.99,
+    price: 0.99,
+    rating: 3.5,
+    numberOfReviews: 4,
+    numberOfSeedsPerPack: 10,
+    seedCount: 25,
+    typeOfPlant: "Vegetable",
+    category: "Cucumber",
+  },
+  {
+    id: "4",
+    name: "Pingtung Long Eggplant",
+    description:
+      "This variety is an Asian classic long slim eggplant. Very easy for management. High yield. Cook it or barbecue as you like. There are 1 grams of seeds inside, which is enough to meet the needs of most people for planting in the yard.",
+    images: [
+      "https://i.etsystatic.com/19374939/r/il/b11750/2778210823/il_794xN.2778210823_f2ft.jpg",
+      "https://i.etsystatic.com/19374939/r/il/61826c/2117014978/il_794xN.2117014978_k5rz.jpg",
+      "https://i.etsystatic.com/19374939/r/il/a4b50d/2778210755/il_794xN.2778210755_rduh.jpg",
+    ],
+    oldPrice: 5.99,
+    price: 1.99,
+    rating: 2.5,
+    numberOfReviews: 2,
+    numberOfSeedsPerPack: 50,
+    seedCount: 250,
+    typeOfPlant: "Vegetable",
+    category: "Eggplant",
+  },
+  {
+    id: "5",
+    name: "Black Cherry Tomato",
+    description:
+      'Vigorous regular leaf plants covered in clusters of 1" round cherry tomatoes. Dusky purple-black fruits bursting with rich flavor—sweet and complex.',
+    images: [
+      "https://shop.seedsavers.org/site/img/seo-images/1609-black-cherry-tomato-organic.jpg",
+    ],
+    oldPrice: 2.99,
+    price: 1.99,
+    rating: 3.5,
+    numberOfReviews: 2,
+    numberOfSeedsPerPack: 25,
+    seedCount: 100,
+    typeOfPlant: "Vegetable",
+    category: "Tomato",
+  },
+  {
+    id: "6",
+    name: "Shishito Pepper",
+    description:
+      "Compact, bushy plants produce an abundance of 3.5-4\"\" wrinkled fruits over an extended period. When eaten in the green stage, as is traditional in Japan, peppers are incredibly crunchy with just a hint of heat. When red, the fruits are sweet and slightly hotter but not as crunchy. Perfect for frying, this Japanese heirloom's fruits are usually mild, but an occasional pepper (about one in 10) is hot. Kitazawa Seed Company played an instrumental role in introducing 'Shishito' to the United States seed trade; Seed Savers Exchange acquired the variety in the 1980s from the Urban Farmerseed company of Convent Station, N.J.",
+    images: [
+      "https://shop.seedsavers.org/site/img/seo-images/0286-shishito-pepper.jpg",
+    ],
+    oldPrice: 2.99,
+    price: 1.99,
+    rating: 3.5,
+    numberOfReviews: 5,
+    numberOfSeedsPerPack: 15,
+    seedCount: 100,
+    typeOfPlant: "Vegetable",
+    category: "Pepper",
+  },
+  {
+    id: "7",
+    name: "Burgess Buttercup Squash",
+    description:
+      "Introduced in 1952 by Burgess Seed & Plant Co. This is a bush version of the original Buttercup variety introduced in 1931. Flattened dark green turbans with a distinctive button on the blossom end. Typical fruits weigh 3-5 pounds. Super sweet brilliant orange flesh with very fine eating qualities. Rind is thin but very hard, medium length keeper.",
+    images: [
+      "https://shop.seedsavers.org/site/img/seo-images/1344-burgess-buttercup-squash-organic.jpg",
+    ],
+    oldPrice: 2.99,
+    price: 1.99,
+    rating: 3.5,
+    numberOfReviews: 1,
+    numberOfSeedsPerPack: 10,
+    seedCount: 25,
+    typeOfPlant: "Vegetable",
+    category: "Squash",
+  },
+  {
+    id: "8",
+    name: "Bloomsdale Spinach",
+    description:
+      "Developed in 1874 by D. Landreth & Sons seed company of Philadelphia. Vigorous upright plants with dark green, glossy, savoyed leaves. Fine quality, very tender, excellent flavor. Quick growing variety with heavy yields. Well adapted for late spring or summer plantings, slow to bolt.",
+    images: [
+      "https://shop.seedsavers.org/site/img/seo-images/0656-Bloomsdale-spinach-Organic.jpg",
+    ],
+    oldPrice: 2.99,
+    price: 1.99,
+    rating: 4.5,
+    numberOfReviews: 4,
+    numberOfSeedsPerPack: 25,
+    seedCount: 50,
+    typeOfPlant: "Vegetable",
+    category: "Spinach",
+  },
+];
+
+export default products;
