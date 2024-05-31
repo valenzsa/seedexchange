4ecfac3ed767f232242a676ad1924e606a161c77
valenzsa <valenzsa418@gmail.com>
2024-05-30 23:41:04 -0400

    Setting up the backend api - Be able to retrieve all products and single product based on product id

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
+import products from "./data/products.js";
 
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,15 @@ const app = express();
 
 app.get("/", (request, response) => {
   response.send("API is running!");
+});
+
+app.get("/products", (request, response) => {
+  response.json(products);
+});
+
+app.get("/products/:id", (request, response) => {
+  let product = products.find((product) => product.id === request.params.id);
+  response.json(product);
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
