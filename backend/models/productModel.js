62fb9210397484dcb2b27d83cf3561529295878c
valenzsa <valenzsa418@gmail.com>
2024-06-05 23:07:40 -0400

    Update productModel to change shortDescription from required true to false

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -38,7 +38,7 @@ const productSchema = new mongoose.Schema(
     },
     shortDescription: {
       type: String,
-      required: true,
+      required: false,
     },
     description: {
       type: String,
