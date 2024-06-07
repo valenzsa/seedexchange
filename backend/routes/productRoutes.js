e2e06e99108f27283f1c2739ccfc33a748ec5848
valenzsa <valenzsa418@gmail.com>
2024-06-07 00:18:53 -0400

    Created asyncHandler via middleware directory.  asyncHandler is added to be able to handle exceptions inside of async routes and passes them to the express error handles

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,15 +1,31 @@
-import express from 'express';
-import products from "../data/products.js";
+import express from "express";
+//import products from "../data/products.js";
 
 const router = express.Router();
 
-router.get("/", (request, response) => {
-  response.json(products);
-});
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
 
-router.get("/:id", (request, response) => {
-  let product = products.find((product) => product.id === request.params.id);
-  response.json(product);
-});
+router.get(
+  "/",
+  asyncHandler(async (request, response) => {
+    const products = await Product.find({});
+    response.json(products);
+  })
+);
+
+router.get(
+  "/:id",
+  asyncHandler(async (request, response) => {
+    // let product = products.find((product) => product.id === request.params.id);
+    const product = await Product.findById(request.params.id);
+
+    if (product) {
+      response.json(product);
+    }
+
+    response.status(404).json({ message: "Product not found" });
+  })
+);
 
 export default router;
