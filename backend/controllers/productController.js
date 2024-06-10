66a97ddeefa9efa7c7dd4cd047582d4de7b227e5
valenzsa <valenzsa418@gmail.com>
2024-06-10 00:14:18 -0400

    Just saving productController.js file

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,11 +1,17 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
 
+// @desc Fetch all products
+// @route GET /api/products
+// @access Public
 const getProducts = asyncHandler(async (request, response) => {
   const products = await Product.find({});
   response.json(products);
 });
 
+// @desc Fetch single product by id
+// @route GET /api/products/:id
+// @access Public
 const getProductById = asyncHandler(async (request, response) => {
   const product = await Product.findById(request.params.id);
 
