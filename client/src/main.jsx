3ca8bf2f8c2a546b35e1511e1f1504764c080dd5
valenzsa <valenzsa418@gmail.com>
2024-06-11 22:47:32 -0400

    Setup redux-toolkit as well as creating constant variables and slices

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store.js";
 import App from "./App.jsx";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -30,6 +32,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
