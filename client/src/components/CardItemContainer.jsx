c123dcfdd0fc1e97ee0edfc7998fd9891b7eb8c1
valenzsa <valenzsa418@gmail.com>
2024-06-13 00:46:42 -0400

    Remove and replaced useState and useEffect.  Use RTK query to retrieve products

--- client/src/components/CardItemContainer.jsx
+++ client/src/components/CardItemContainer.jsx
@@ -1,34 +1,26 @@
-import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import axios from "axios";
 import CardItem from "./CardItem";
 //import products from "../products";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 const CardItemContainer = () => {
-  const [products, setProducts] = useState([]);
-
-  const getProducts = async () => {
-    try {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getProducts();
-  }, []);
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <Container>
-      <Row className="justify-content-md-center mb-5">
-        {products.map((product) => (
-          <Col lg={3} md={4} sm={6} xs={12} className="mb-3" key={product.id}>
-            <CardItem {...product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <Row className="justify-content-md-center mb-5">
+          {products.map((product) => (
+            <Col lg={3} md={4} sm={6} xs={12} className="mb-3" key={product.id}>
+              <CardItem {...product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </Container>
   );
 };
